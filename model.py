5230eb0bb17161ac4c04ccd37b5409d69caa8767
Samuel <samuelsousa.mg@diarioassociados.com.br>
2018-03-13 16:45:54 -0300

    created database model

---
+++ model.py
@@ -0,0 +1,17 @@
+from google.appengine.ext import ndb
+
+
+class User(ndb.Model):
+    """Sub model for representing an user."""
+    name = ndb.StringProperty()
+    email = ndb.StringProperty()
+
+
+class Purchase(ndb.Model):
+    """Sub model for representing a purchase."""
+    product = ndb.StringProperty()
+    brand = ndb.StringProperty()
+    price = ndb.FloatProperty()
+    unity = ndb.StringProperty()
+    purchase_date = ndb.DateTimeProperty(auto_now_add=True)
+    user = ndb.StructuredProperty(User)
