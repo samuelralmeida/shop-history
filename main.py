a7a2702bf31a1f62828ac6d895c2e4254652791d
Samuel <samuelsousa.mg@diarioassociados.com.br>
2018-03-14 09:52:05 -0300

    create routes to app

--- main.py
+++ main.py
@@ -1,7 +1,76 @@
+# encoding: utf-8
+
+import random
+import string
+
+from google.appengine.api import users
+
 from flask import Flask, render_template, request
+from flask import session as login_session
 
 app = Flask(__name__)
 
+
+def get_user():
+    users.get_current_user()
+
+
+@app.route('/')
+@app.route('/login')
+def show_login():
+    # state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+    #                 for x in xrange(32))
+    # login_session['state'] = state
+    # return render_template('login.html', STATE=state)
+    return 'pagina inicial e login'
+
+
+@app.route('/logout')
+def logout():
+    return 'logout'
+
+
+@app.route('/prepare')
+def prepare_list():
+    return 'preparar lista'
+
+
+@app.route('/market')
+def market():
+    return 'mercado'
+
+
+@app.route('/create/product')
+def create_product():
+    return 'criar produto'
+
+
+@app.route('/edit/product')
+def edit_product():
+    return 'editar produto'
+
+
+@app.route('/delete/product')
+def delete_product():
+    return 'deletar produto'
+
+
+@app.route('/make/purchase')
+def make_purchase():
+    return 'fazer purchase'
+
+
+@app.route('/edit/purchase')
+def edit_purchase():
+    return 'editar purchase'
+
+
+@app.route('/delete/purchase')
+def delete_purchase():
+    return 'deletar purchase'
+
+
+"""
 @app.route('/form')
 def form():
     return render_template('form.html')
@@ -20,3 +89,4 @@ def submitted_form():
         email=email,
         site=site,
         comments=comments)
+"""
